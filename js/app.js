e8e5d786cb35c1ea9f8533d7ccd2784f9f0e8148
Camille <aziaziazi@gmail.com>
2017-05-29 01:51:08 +0200

    implement player move

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ var Enemy = function() {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
+        // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
 };
@@ -30,16 +30,36 @@ Enemy.prototype.render = function() {
 
 var Player = function() {
     this.sprite = 'images/char-boy.png';
+    this.x = 0;
+    this.y = 300;
 };
 
 Player.prototype.update = function(dt) {
+    // this.x = this.x+1
+    // console.log('this.x =>', this.x);
 };
 
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-
+Player.prototype.handleInput = function(pressedKey) {
+    console.log('pressedKey =>', pressedKey);
+    switch(pressedKey){
+        case "left":
+            this.x = this.x - 100;
+            break;
+        case "up":
+            this.y = this.y - 85;
+            break;
+        case "right":
+            this.x = this.x + 100;
+            break;
+        case "down":
+            this.y = this.y + 85;
+            break;
+    }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
@@ -53,7 +73,7 @@ var player = new Player()
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keydown', function(e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
